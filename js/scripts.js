c3fd5c277361aa45ece132d4e2c355d8ddfa676a
Anderson Aparecido Hattore de Araujo <andersonhattore@hotmail.com>
2022-07-12 12:23:19 -0300

    Projeto concluído

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 let x = document.querySelector(".x");
 let o = document.querySelector(".o");
 let boxes = document.querySelectorAll(".box");
-let button = document.querySelectorAll("#buttons-container button");
+let buttons = document.querySelectorAll("#buttons-container button");
 let messageContainer = document.querySelector('#message');
 let messageText = document.querySelector('#message p');
 let secondPlayer;
@@ -32,6 +32,18 @@ for(let i = 0; i < boxes.length;i++){
 
             if(player1 == player2) {
                 player1++;
+
+                if(secondPlayer == 'ai-player'){
+                    
+                    //função executar jogada
+                    computerPlay();
+
+                    player2++;
+
+                
+
+                }
+
             }else{
                 player2++;
             }
@@ -43,6 +55,33 @@ for(let i = 0; i < boxes.length;i++){
     });
 
 }
+
+//evento para saber se é 2 players ou IA
+
+for(let i = 0; i < buttons.length;i++){
+
+    buttons[i].addEventListener("click",function(){
+
+        secondPlayer = this.getAttribute("id");
+
+        for (let j= 0; j < buttons.length; j++){
+
+            buttons[j].style.display = 'none';
+
+        }
+
+        setTimeout(function(){
+            let container = document.querySelector("#container");
+            container.classList.remove("hide");
+
+
+        },500);
+
+    });
+
+}
+
+
 //ver quem vai jogar
 function checkEl(player1,player2){
     if(player1 == player2){
@@ -196,21 +235,23 @@ function checkWinCondition() {
             declareWinner('o');
         }   
     }
+
 //deu velha
 
-let counter=0;
+let counter= 0;
 
-for(let i=0; i<boxes.length; i++){
-    if(boxes[1].childNodes[0] != undefined){
+for(let i = 0; i < boxes.length; i++){
+    if(boxes[i].childNodes[0] != undefined){
     counter++;
     }
 }
 
-if(counter == 10){
+if(counter == 9){
     declareWinner('deu velha');
 }
 
 }
+
 
 //Limpa o jogo, declara o vencedor e atualiza o placar
 
@@ -228,7 +269,7 @@ function declareWinner(winner){
         scoreboardY.textContent = parseInt(scoreboardY.textContent)+ 1 ;
         msg ='O jogador 2 venceu';
     } else {
-        msg="teste";
+        msg="Deu velha";
     }
 
 //exibe msg
@@ -246,7 +287,7 @@ setTimeout(function(){
 player1 = 0;
 player2 = 0;
 
-//remove x e o o
+//remove x e o
 
 let boxesToRemove = document.querySelectorAll(".box div");
 
@@ -254,4 +295,34 @@ for(let i = 0 ; i< boxesToRemove.length; i++){
     boxesToRemove[i].parentNode.removeChild(boxesToRemove[i]);
 }
 
+}
+
+// executa  a lógica da jogada da IA
+
+function computerPlay(){
+
+    let cloneO = o.cloneNode(true);
+    counter = 0;
+    filled = 0;
+
+    for(let i = 0; i< boxes.length; i++){
+
+        let randomNumber = Math.floor(Math.random() * 5);
+
+        // só preencher se estiver vazio o filho
+        if (boxes[i].childNodes[0] == undefined) {
+            if(randomNumber <= 1) {
+                boxes[i].appendChild(cloneO);
+                counter++;               
+                break;
+            }
+           
+        //checagem de quantas estão preenchidas
+        } else{
+            filled++;
+        }
+    }
+    if(counter == 0 && filled < 9){
+        computerPlay();
+    }
 }
